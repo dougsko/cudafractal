432f1fa7d8d4a05ddc4fb7bcf57dca1c27b458ba
Doug P <dougtko@gmail.com>
2012-08-01 13:26:08 -0400

    added .h files from book

--- cudafractal.cu
+++ cudafractal.cu
@@ -1,8 +1,27 @@
 #include <cuda.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <gd.h>
 
 #define DIM 1000
+
+int
+makePNG(char *imgData)
+{
+    gdImagePtr image;
+    FILE *out;
+    int color;
+
+    image = gdImageCreate(DIM, DIM);
+    color = gdImageColorAllocate(image, 0, 0, 0); // R, G, B
+    gdImageSetPixel(image, 0, 0, color);
+    out = fopen("foo.png", "wb");
+    gdImagePng(image, out);
+    fclose(out);
+    gdImageDestroy(image);
+
+    return 0;
+}
 
 struct 
 cuComplex
