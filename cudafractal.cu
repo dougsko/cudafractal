7dd5436e81bd62dad99602b9195bc1d44d9c67e1
Doug P <dougtko@gmail.com>
2012-08-03 15:35:48 -0400

    ...

--- cudafractal.cu
+++ cudafractal.cu
@@ -5,7 +5,9 @@
 #include "book.h"
 #include "png_helper.h"
 
-#define DIM 1000
+#define DIM 1080
+#define DIM_X 1920
+#define DIM_Y 1080
 
 
 struct 
@@ -37,9 +39,10 @@ cuComplex
 __device__ int 
 julia(int x, int y)
 {
-    const float scale = 1.5;
-    float jx = scale * (float)(DIM/2 - x)/(DIM/2);
-    float jy = scale * (float)(DIM/2 - y)/(DIM/2);
+    const float scale = 0.8;
+    float jx = scale * (float)(DIM_X/2 - x)/(DIM_X/2);
+    float jy = scale * (float)(DIM_Y/2 - y)/(DIM_Y/2);
+    float mag;
 
     cuComplex c(-0.8, 0.156);
     cuComplex a(jx, jy);
@@ -48,8 +51,9 @@ julia(int x, int y)
     for(i = 0; i < 200; i++)
     {
         a = a * a + c;
-        if(a.magnatude2() > 1000)
-            return 0;
+        mag = a.magnatude2();
+        if(mag > 1000)
+            return mag;
     }
     return 1;
 }
@@ -62,7 +66,10 @@ kernel(unsigned char *ptr)
     int offset = x + y * gridDim.x;
 
     int juliaValue = julia(x, y);
-    ptr[offset*4 + 0] = 255 * juliaValue;
+    if(juliaValue == 1)
+        ptr[offset*4 + 0] = 255 * juliaValue;
+    else
+        ptr[offset*4 + 0] = 0;
     ptr[offset*4 + 1] = 0;
     ptr[offset*4 + 2] = 0;
     ptr[offset*4 + 3] = 255;
@@ -72,17 +79,17 @@ int
 main(void)
 {
     unsigned char *dev_bitmap;
-    CPUBitmap bitmap(DIM, DIM);
+    CPUBitmap bitmap(DIM_X, DIM_Y);
 
 
     cudaMalloc((void **) &dev_bitmap, bitmap.image_size());
 
-    dim3 grid(DIM, DIM);
+    dim3 grid(DIM_X, DIM_Y);
     kernel<<<grid, 1>>>(dev_bitmap);
 
     cudaMemcpy(bitmap.get_ptr(), dev_bitmap, bitmap.image_size(), cudaMemcpyDeviceToHost);
 
-//    bitmap.display_and_exit();
+    bitmap.display_and_exit();
 
     cudaFree(dev_bitmap);
 
